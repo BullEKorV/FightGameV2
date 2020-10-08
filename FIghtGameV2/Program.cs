df1079b31c51806dbbd22c5466c94ee028d3df87
Elias <elias.wasielewski@elev.ga.ntig.se>
2020-10-08 22:54:34 +0200

    more big changes

--- FIghtGameV2/Program.cs
+++ FIghtGameV2/Program.cs
@@ -6,8 +6,8 @@ namespace Fight
     class Program
     {
         static Random rnd = new Random();
-        static int round;
         static bool versus;
+        static bool player1Turn;
         static string p1Name;
         static string p2Name;
         static int p1Health;
@@ -20,77 +20,58 @@ namespace Fight
             while (!endApp)
             {
                 //reset game data
-                round = 1;
+                player1Turn = true;
                 p1Health = 50;
                 p2Health = 50;
                 gameActive = true;
 
                 Console.WriteLine("Welcome to fight!");
                 Console.WriteLine("Do you want to play agains a Bot or another player? Bot/Player?");
-                {
-                    string response = Console.ReadLine().ToLower();
-                    while (!(response == "bot" || response == "player"))
-                    {
-                        Console.WriteLine("Please enter Bot/Player");
-                        response = Console.ReadLine().ToLower();
-                    }
-                    if (response == "player") versus = true;
-                    else
-                    {
-                        versus = false;
-                        p2Name = "Opponent";
-                    }
-                }
                 //setup names
-                Console.WriteLine("Enter Player 1's name");
-                p1Name = Console.ReadLine();
-                while (p1Name.Length > 12 || p1Name.Length < 3)
-                {
-                    Console.WriteLine("Name cannot be shorter than 3 characters or longer than 12.");
-                    p1Name = Console.ReadLine();
-                }
-                if (versus)
-                {
-                    Console.WriteLine("Enter Player 2's name");
-                    p2Name = Console.ReadLine();
-                    while (p2Name.Length > 12 || p2Name.Length < 3)
-                    {
-                        Console.WriteLine("Name cannot be shorter than 3 characters or longer than 12.");
-                        p2Name = Console.ReadLine();
-                    }
-                }
-                Console.ReadKey();
+                PlayersSetup();
 
                 while (gameActive)
                 {
-                    Console.Clear();
-                    Console.WriteLine($"Round {round}!");
-                    //player 1
-                    Console.WriteLine($"{p1Name}'s turn!");
-                    //player damage calc
-                    int damage = PlayerAttack();
-                    p2Health -= damage;
-
-                    if (damage != 0) Console.WriteLine($"\n{p1Name} hit {p2Name} for {damage} damage\n");
-                    Console.WriteLine($"{p2Name} health remaining: {p2Health}\n");
-                    CheckLose();
-                    if (gameActive == false) break;
-                    Console.ReadKey();
-
-                    //player 2 or bot
-                    Console.WriteLine($"{p2Name}'s turn!");
-                    //player damage calc
-                    if (versus) damage = PlayerAttack();
-                    else damage = BotAttack();
-                    p1Health -= damage;
-
-                    if (damage != 0) Console.WriteLine($"\n{p2Name} hit {p1Name} for {damage} damage\n");
-                    Console.WriteLine($"{p1Name} health remaining: {p1Health}\n");
-                    CheckLose();
-                    if (gameActive == false) break;
-                    Console.ReadKey();
-                    round++;
+                    GameLogic();
                 }
+            }
+        }
+        static void StatusBar()
+        {
+            Console.Clear();
+            Console.WriteLine($"{p1Name}'s health {p1Health},        {p2Name}'s health {p2Health}\n");
+        }
+        static void GameLogic()
+        {
+            int damage;
+            //player 1
+            if (player1Turn)
+            {
+                StatusBar();
+                Console.WriteLine($"{p1Name}'s turn!");
+                //player damage calc
+                damage = PlayerAttack();
+                p2Health -= damage;
+
+                if (damage != 0) Console.WriteLine($"\n{p1Name} hit {p2Name} for {damage} damage\n");
+                Console.WriteLine($"{p2Name} health remaining: {p2Health}\n");
+                CheckLose();
+                Console.ReadKey();
+            }
+            //player 2 or bot
+            else if (!player1Turn)
+            {
+                StatusBar();
+                Console.WriteLine($"{p2Name}'s turn!");
+                //player damage calc
+                if (versus) damage = PlayerAttack();
+                else damage = BotAttack();
+                p1Health -= damage;
+
+                if (damage != 0) Console.WriteLine($"\n{p2Name} hit {p1Name} for {damage} damage\n");
+                Console.WriteLine($"{p1Name} health remaining: {p1Health}\n");
+                CheckLose();
+                Console.ReadKey();
             }
         }
         static int PlayerAttack()
@@ -100,9 +81,11 @@ namespace Fight
             string attackType = Console.ReadLine().ToLower();
             while (!(attackType == "heavy" || attackType == "light"))
             {
+                StatusBar();
                 Console.WriteLine("Heavy/Light?");
                 attackType = Console.ReadLine().ToLower();
             }
+            StatusBar();
             //light attack
             if (attackType == "light")
             {
@@ -110,7 +93,13 @@ namespace Fight
                 int hitChance = rnd.Next(0, 10);
                 if (hitChance < 9)
                 {
-                    damage = rnd.Next(1, 5);
+                    damage = rnd.Next(2, 5);
+                    int critChance = rnd.Next(0, 8);
+                    if (critChance == 0)
+                    {
+                        Console.WriteLine(" resulting in critical damage!");
+                        damage *= 2;
+                    }
                     return damage;
                 }
                 else Console.WriteLine(", but missed!");
@@ -122,7 +111,13 @@ namespace Fight
                 int hitChance = rnd.Next(0, 15);
                 if (hitChance < 8)
                 {
-                    damage = rnd.Next(5, 10);
+                    damage = rnd.Next(5, 8);
+                    int critChance = rnd.Next(0, 12);
+                    if (critChance == 0)
+                    {
+                        Console.WriteLine(" resulting in critical damage!");
+                        damage *= 2;
+                    }
                     return damage;
                 }
                 else Console.WriteLine(", but missed!");
@@ -141,6 +136,12 @@ namespace Fight
                 if (hitChance < 9)
                 {
                     damage = rnd.Next(1, 5);
+                    int critChance = rnd.Next(0, 8);
+                    if (critChance == 0)
+                    {
+                        Console.WriteLine(" resulting in critical damage!");
+                        damage *= 2;
+                    }
                     return damage;
                 }
                 else Console.WriteLine(", but missed!");
@@ -153,6 +154,12 @@ namespace Fight
                 if (hitChance < 8)
                 {
                     damage = rnd.Next(5, 10);
+                    int critChance = rnd.Next(0, 12);
+                    if (critChance == 0)
+                    {
+                        Console.WriteLine(" resulting in critical damage!");
+                        damage *= 2;
+                    }
                     return damage;
                 }
                 else Console.WriteLine(", but missed!");
@@ -184,6 +191,39 @@ namespace Fight
                 }
                 if (response == "n") endApp = true;
                 gameActive = false;
+            }
+            else player1Turn = !player1Turn;
+        }
+        static void PlayersSetup()
+        {
+            string response = Console.ReadLine().ToLower();
+            while (!(response == "bot" || response == "player"))
+            {
+                Console.WriteLine("Please enter Bot/Player");
+                response = Console.ReadLine().ToLower();
+            }
+            if (response == "player") versus = true;
+            else
+            {
+                versus = false;
+                p2Name = "Opponent";
+            }
+            Console.WriteLine("Enter Player 1's name");
+            p1Name = Console.ReadLine();
+            while (p1Name.Length > 12 || p1Name.Length < 3)
+            {
+                Console.WriteLine("Name cannot be shorter than 3 characters or longer than 12.");
+                p1Name = Console.ReadLine();
+            }
+            if (versus)
+            {
+                Console.WriteLine("Enter Player 2's name");
+                p2Name = Console.ReadLine();
+                while (p2Name.Length > 12 || p2Name.Length < 3)
+                {
+                    Console.WriteLine("Name cannot be shorter than 3 characters or longer than 12.");
+                    p2Name = Console.ReadLine();
+                }
             }
         }
     }
